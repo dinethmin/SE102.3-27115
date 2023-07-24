628e9c99f902a5d2a54f3cfad0ca6d16858b5efb
dinethmin <chanudadinethmin@gmail.com>
2023-07-24 21:39:17 +0530

    tutorial 5

---
+++ tutorial 5/j.js
@@ -0,0 +1,11 @@
+var num1 = parseInt(prompt("Enter number 1:" , "number"));
+var num2 = parseInt(prompt("Enter number 2:" , "number"));
+
+
+document.write("Addition is: ",num1+num2);
+
+document.write("<br>subtraction is: ",num1-num2);
+
+document.write("<br>Multipication is: ",num1*num2);
+
+document.write("<br>Divition is: ",num1/num2);
